b7621a321067d5868871bc9affb5a4e0ce8bf154
SeptiZahrotunNisa <szahrotunn1009@gmail.com>
2018-10-31 13:36:45 +0700

    soal2

---
+++ soal2/soal2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int N,X,T,Batas;
+
+    cout << "masukan bilangan N: ";
+    cin >> N;
+
+
+    Batas = N + 140;
+    X = 20;
+    T = N;
+
+    while( T <= Batas)
+        { T = T + X;
+          X = X + 10;
+    }
+    cout << T;
+
+}
